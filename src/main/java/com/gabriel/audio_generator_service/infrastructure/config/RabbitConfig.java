b7807c2ba0cb122ffb8ab4dd930ef48a7c19680c
Gabriel Soares <gabrielalcantarars@gmail.com>
2024-11-23 12:53:37 -0300

    feat: Setting MessageConverter as part of RabbitTemplate to serialize the message correctly before sending to the consumer
    - rabbitTemplate method from RabbitConfig.java class now depends on MessageConverter to build final RabbitTemplate object

--- src/main/java/com/gabriel/audio_generator_service/infrastructure/config/RabbitConfig.java
+++ src/main/java/com/gabriel/audio_generator_service/infrastructure/config/RabbitConfig.java
@@ -8,6 +8,7 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,7 +32,7 @@ public class RabbitConfig {
     private String rabbitPassword;
 
     @Bean
-    public Jackson2JsonMessageConverter messageConverter() {
+    public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
@@ -62,8 +63,10 @@ public class RabbitConfig {
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        return new RabbitTemplate(connectionFactory);
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
     }
 
 }
