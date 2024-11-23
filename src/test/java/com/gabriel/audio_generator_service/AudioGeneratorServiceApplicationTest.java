db1576e01bf904915e2ed1facb52493986f143bf
Gabriel Soares <gabrielalcantarars@gmail.com>
2024-11-23 19:22:40 -0300

    test: Add unit test for AudioGeneratorController to verify audio generation functionality
    - Mocked AudioGenerationFacade and tested the generateAudios method in the controller.
    - Ensured that the controller returns the expected AudioGeneratorResponse.
    - Followed Arrange, Act, Assert testing pattern for clear and structured testing.

---
+++ src/test/java/com/gabriel/audio_generator_service/AudioGeneratorServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gabriel.audio_generator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioGeneratorServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
