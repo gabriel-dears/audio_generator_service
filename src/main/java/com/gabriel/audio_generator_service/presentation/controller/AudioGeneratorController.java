46ade5191c457de117e256e560d92cc1ef88c39a
Gabriel Soares <gabrielalcantarars@gmail.com>
2024-11-23 19:03:34 -0300

    refactor: Remove @Autowired and inject AudioGenerationFacade via constructor
    - Replaced field injection with constructor injection for better testability and immutability.
    - Removed the use of @Autowired annotation on the AudioGenerationFacade field.

--- src/main/java/com/gabriel/audio_generator_service/presentation/controller/AudioGeneratorController.java
+++ src/main/java/com/gabriel/audio_generator_service/presentation/controller/AudioGeneratorController.java
@@ -3,7 +3,6 @@ package com.gabriel.audio_generator_service.presentation.controller;
 import com.gabriel.audio_generator_service.application.dto.AudioGeneratorRequest;
 import com.gabriel.audio_generator_service.application.dto.AudioGeneratorResponse;
 import com.gabriel.audio_generator_service.application.facade.AudioGenerationFacade;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/generate-audio")
 public class AudioGeneratorController {
 
-    @Autowired
-    private AudioGenerationFacade audioGenerationFacade;
+    private final AudioGenerationFacade audioGenerationFacade;
+
+    public AudioGeneratorController(AudioGenerationFacade audioGenerationFacade) {
+        this.audioGenerationFacade = audioGenerationFacade;
+    }
 
     @PostMapping("/channel")
     public AudioGeneratorResponse generateAudios(@RequestBody AudioGeneratorRequest audioGeneratorRequest) {
